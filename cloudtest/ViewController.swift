3a6e415aeca6762af70d740d62914b08005210c0
Valery Silin <skelet2394@gmail.com>
2022-12-01 18:12:16 +0400

    Initial Commit

---
+++ cloudtest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  cloudtest
+//
+//  Created by Valery Silin on 01.12.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
